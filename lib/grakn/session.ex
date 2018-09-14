9ba912bc7d39b4986dcbb4793df7122bfad14ba4
Alex <alexander@taxfix.de>
2018-09-14 20:43:23 +0200

    Added typespecs in session module

--- lib/grakn/session.ex
+++ lib/grakn/session.ex
@@ -1,9 +1,13 @@
 defmodule Grakn.Session do
+  @opaque t :: pid()
+
+  @spec start_link(String.t()) :: {:ok, t()} | {:error, any()}
   def start_link(uri) do
     {:ok, channel} = GRPC.Stub.connect(uri)
     Agent.start_link(fn -> channel end)
   end
 
+  @spec transaction(t()) :: {:ok, Grakn.Transaction.t()} | {:error, any()}
   def transaction(session) do
     session
     |> get_channel

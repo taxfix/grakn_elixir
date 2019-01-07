5624e75807c8547f79880bd8fc683ffe969f1e35
Alex <alexander@taxfix.de>
2019-01-07 18:04:51 +0100

    Allow define statements on subtypes

--- lib/grakn/graql.ex
+++ lib/grakn/graql.ex
@@ -2,16 +2,14 @@ defmodule Grakn.Graql do
   alias Grakn.Query
 
   defmodule Datatypes do
-    defstruct [
-      string: :string,
-      long: :long,
-      double: :double,
-      boolean: :boolean,
-      date: :date
-    ]
+    defstruct string: :string,
+              long: :long,
+              double: :double,
+              boolean: :boolean,
+              date: :date
   end
 
-  defmacro  __using__([]) do
+  defmacro __using__([]) do
     quote do
       import Grakn.Graql
     end
@@ -19,16 +17,29 @@ defmodule Grakn.Graql do
 
   def datatypes, do: %Datatypes{}
 
-  @spec define(String.t() | atom(), keyword()) :: Query.t()
   defmacro define(label, [sub: :entity] = opts), do: define_body(label, opts)
   defmacro define(label, [sub: :entity, has: _] = opts), do: define_body(label, opts)
   defmacro define(label, [sub: :entity, plays: _] = opts), do: define_body(label, opts)
   defmacro define(label, [sub: :entity, has: _, plays: _] = opts), do: define_body(label, opts)
+  defmacro define(label, [sub: :entity, plays: _, has: _] = opts), do: define_body(label, opts)
   defmacro define(label, sub: :attribute), do: define_body(label, sub: :attribute)
   defmacro define(label, [sub: :attribute, datatype: _] = opts), do: define_body(label, opts)
-  defmacro define(label, [sub: :relationship, relates: _] = opts), do:  define_body(label, opts)
-  defmacro define(label, [sub: :relationship, relates: _, has: _] = opts), do: define_body(label, opts)
-  defmacro define(label, opts), do: throw "Graql compile error: #{inspect({label, opts})}"
+  defmacro define(label, [sub: :relationship, relates: _] = opts), do: define_body(label, opts)
+
+  defmacro define(label, [sub: :relationship, relates: _, has: _] = opts),
+    do: define_body(label, opts)
+
+  # Allow any arbitrary sub types
+  defmacro define(label, [sub: _type] = opts), do: define_body(label, opts)
+  defmacro define(label, [sub: _type, has: _] = opts), do: define_body(label, opts)
+  defmacro define(label, [sub: _type, plays: _] = opts), do: define_body(label, opts)
+  defmacro define(label, [sub: _type, has: _, plays: _] = opts), do: define_body(label, opts)
+  defmacro define(label, [sub: _type, plays: _, has: _] = opts), do: define_body(label, opts)
+  defmacro define(label, [sub: _type, relates: _] = opts), do: define_body(label, opts)
+
+  defmacro define(label, opts), do: error(label, opts)
+
+  defp error(label, opts), do: raise("Graql compile error: #{inspect({label, opts})}")
 
   defp define_body(label, opts) do
     quote do
@@ -36,14 +47,16 @@ defmodule Grakn.Graql do
         unquote(opts)
         |> Enum.map(&expand_key_values/1)
         |> Enum.join(", ")
+
       Query.graql("define #{unquote(label)} #{modifiers};")
     end
   end
 
-  def expand_key_values({key, [_|_] = values}) do
+  def expand_key_values({key, [_ | _] = values}) do
     values
     |> Enum.map(fn value -> "#{key} #{value}" end)
     |> Enum.join(", ")
   end
+
   def expand_key_values({key, value}), do: "#{key} #{value}"
 end
--- test/grakn/graql_test.exs
+++ test/grakn/graql_test.exs
@@ -3,55 +3,84 @@ defmodule Grakn.GraqlTest do
   use Grakn.Graql
 
   test "define an attribute" do
-    %{graql: graql} =
-      define "att", sub: :attribute
+    %{graql: graql} = define("att", sub: :attribute)
     assert graql === "define att sub attribute;"
   end
 
   test "define an attribute with datatype" do
-    %{graql: graql} =
-      define "att", sub: :attribute, datatype: datatypes().string
+    %{graql: graql} = define("att", sub: :attribute, datatype: datatypes().string)
     assert graql === "define att sub attribute, datatype string;"
   end
 
   test "define an entity" do
-    %{graql: graql} = define "something", sub: :entity
+    %{graql: graql} = define("something", sub: :entity)
     assert graql === "define something sub entity;"
   end
 
   test "define an entity with attribute" do
     %{graql: graql} =
-      define "something", sub: :entity,
+      define("something",
+        sub: :entity,
         has: "att"
+      )
+
     assert graql === "define something sub entity, has att;"
   end
 
   test "define an entity with multiple attributes" do
     %{graql: graql} =
-      define "something", sub: :entity,
+      define("something",
+        sub: :entity,
         has: ["att1", "att2"]
+      )
+
     assert graql === "define something sub entity, has att1, has att2;"
   end
 
   test "define a relationship" do
-    %{graql: graql} =
-      define "r", sub: :relationship, relates: ["x", "y"]
+    %{graql: graql} = define("r", sub: :relationship, relates: ["x", "y"])
     assert graql === "define r sub relationship, relates x, relates y;"
   end
 
   test "define a relationship with attribute" do
     %{graql: graql} =
-      define "r", sub: :relationship,
+      define("r",
+        sub: :relationship,
         relates: ["x", "y"],
         has: "att1"
+      )
+
     assert graql === "define r sub relationship, relates x, relates y, has att1;"
   end
 
   test "define a relationship with multiple attributes" do
     %{graql: graql} =
-      define "r", sub: :relationship,
+      define("r",
+        sub: :relationship,
         relates: ["x", "y"],
         has: ["att1", "att2"]
+      )
+
     assert graql === "define r sub relationship, relates x, relates y, has att1, has att2;"
+  end
+
+  test "define a entity subtype" do
+    %{graql: graql} = define("user", sub: "person")
+    assert graql === "define user sub person;"
+
+    %{graql: graql} = define("user", sub: "person", has: ["name"])
+    assert graql === "define user sub person, has name;"
+
+    %{graql: graql} = define("user", sub: "person", plays: ["actor"])
+    assert graql === "define user sub person, plays actor;"
+
+    %{graql: graql} = define("user", sub: "person", has: ["name"])
+    assert graql === "define user sub person, has name;"
+
+    %{graql: graql} = define("user", sub: "person", has: ["name"], plays: ["actor"])
+    assert graql === "define user sub person, has name, plays actor;"
+
+    %{graql: graql} = define("user", sub: "person", plays: ["actor"], has: ["name"])
+    assert graql === "define user sub person, plays actor, has name;"
   end
 end

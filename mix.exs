453c07f8dcecb593796a9b96ddf4fd2fcb1008cb
Marco Milanesi <kpanic@gmail.com>
2019-01-13 11:30:29 +0100

    Specify folder for plt_file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule GraknElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_file: {:no_warn, ".dialyzer/local.plt"}]
     ]
   end
 

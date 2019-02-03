232be0920dd5a129912e807076b2c2d0411d9d21
Alex <alexander@taxfix.de>
2019-02-03 18:40:06 +0100

    Fixed specs

--- lib/grakn/concept/schema_concept.ex
+++ lib/grakn/concept/schema_concept.ex
@@ -7,12 +7,13 @@ defmodule Grakn.Concept.SchemaConcept do
     DBConnection.execute(conn, Action.get_schema_concept(), [label], opts)
   end
 
-  @spec get(Concept.t(), DBConnection.t(), keyword()) :: {:ok, String.t()} | {:error, any()}
+  @spec label(Concept.t(), DBConnection.t(), keyword()) :: {:ok, String.t()} | {:error, any()}
   def label(%{id: concept_id}, conn, opts \\ []) do
     DBConnection.execute(conn, Action.concept_label(), [concept_id], opts)
   end
 
-  @spec get(Concept.t(), DBConnection.t(), keyword()) :: {:ok, [Concept.t()]} | {:error, any()}
+  @spec attribute_types(Concept.t(), DBConnection.t(), keyword()) ::
+          {:ok, [Concept.t()]} | {:error, any()}
   def attribute_types(%{id: concept_id}, conn, opts \\ []) do
     DBConnection.execute(conn, Action.get_attribute_types(), [concept_id], opts)
   end

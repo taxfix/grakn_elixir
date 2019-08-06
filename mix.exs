8ade4fd13ca561f4c9f2f53d45ed2d267b0298b3
Marco Milanesi <marco@taxfix.de>
2019-08-06 13:30:20 +0200

    Bump dialyxir

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule GraknElixir.MixProject do
       {:protobuf, "~> 0.5.3"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:benchee, "~> 0.13", only: :dev}
     ]

b1080f1caabb06fada496cabb531cbae67ea62b0
Marco Milanesi <marco@taxfix.de>
2019-08-06 14:04:51 +0200

    Ignore ssl

--- mix.exs
+++ mix.exs
@@ -12,7 +12,10 @@ defmodule GraknElixir.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      dialyzer: [plt_file: {:no_warn, ".dialyzer/local.plt"}]
+      dialyzer: [
+        plt_file: {:no_warn, ".dialyzer/local.plt"},
+        ignore_warnings: "dialyzer_ignore.exs"
+      ]
     ]
   end
 
